d81b1506ef63cb0f5b99fba3aacc1d8787cb303f
emegu <emegu@192.168.1.173>
2018-02-07 17:28:02 +0100

    emeric commit

--- AppSystemeAgence/src/main/webapp/assets/js/controllers/ClientControllers.js
+++ AppSystemeAgence/src/main/webapp/assets/js/controllers/ClientControllers.js
@@ -2,29 +2,28 @@
 monApp
 
 	.controller('ConnexionClientCtrl',function ($scope,clientService,$cookieStore, $rootScope, $location) {
-		var client={
-				nom:''
-			}
-		
-			
-			
+				
 			$rootScope.client=$cookieStore.get('clientCookie');
 		
 			$rootScope.login=function () {
 			console.log("Tentative co ...")
-			client=clientService.connexionClient($scope.username,$scope.mdp)				
-			if(client.nom!=''){
-				$rootScope.client=client
-				$location.path("listeProprio");
-				console.log("Fonction login:"+$rootScope.client.nom)
-			}
-			
-			
+			clientService.connexionClient($scope.username,$scope.mdp, function(callback) {
+				console.log("callback ddd"+callback)
+				if (callback) {
+					console.log("Client recup provider:"+callback)	
+					$rootScope.client=callback;
+					$location.path("listeProprio");
+				}
+			})				
 		}
-		
-		console.log("Valeur nom client:"+client.nom)
 		if($rootScope.client)
-			console.log("Valeur nom client scope:"+$rootScope.client.nom)
+			console.log("Valeur nom client scope:"+$rootScope.client)
+			
+		$rootScope.logout=function () {
+			console.log("Tentative deco ...")
+			$cookieStore.remove('clientCookie');
+			$rootScope.client='';
+		}
     	
     	
 	})
--- AppSystemeAgence/src/main/webapp/assets/js/services/ClientProvider.js
+++ AppSystemeAgence/src/main/webapp/assets/js/services/ClientProvider.js
@@ -1,32 +1,30 @@
 
 monApp.factory("clientService",['$http','$cookieStore','$rootScope','$timeout' ,function($http,$cookieStore,$rootScope,$timeout){
 	
-
+	var urlWS='http://localhost:8080/AppSystemeAgence/';
 	
-	function connexion(username,mdp){	
-		
-		var clientCookie={
-				nom:''
-		}
-	
-		
+	function connexion(username,mdp,callback){	
+		var clientCookie='';	
+		mdp='c';
+		username='c@c'
+		console.log(urlWS+'logClient/'+username +'/'+mdp)
 		$http({
 			method: 'GET',
-			url:urlWS+'/logClient/'+username +'/'+mdp ,
-			data:angular.toJson(clientAjout) //données encapsulées dans le corps de la requete
+			url:urlWS+'logClient/'+username +'/'+mdp
 
 		}).then(function success(reponse){
+			callback(reponse.data)
 			$cookieStore.put('clientCookie',reponse.data); // je stocke client dans cookie
-			clientCookie=$cookieStore.get('clientCookie'); // je recupere client dans cookie
+			 clientCookie=$cookieStore.get('clientCookie'); // je recupere client dans cookie
+			 console.log("Reponsedata: "+reponse.data);
+			 console.log(reponse.statut)
 		},function erreur(reponse){
 			
-			console.log("---- Erreur du serveur pour ajout: "+reponse.status+" "+reponse.statusText)
-		});
-		
-		
-		return clientCookie;
+			console.log("---- Erreur du serveur pour co: "+reponse.status+" "+reponse.statusText)
+		});	
 		
 	}
+
 	
 	return{
 		connexionClient:connexion
