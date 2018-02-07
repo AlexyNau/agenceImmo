ea40a2f6bc49f61d822439373e8de7f85b8f6ba5
Adaming44 <Adaming44@192.168.1.145>
2018-02-07 16:07:07 +0100

    ddd

---
+++ AppSystemeAgence/src/main/java/fr/adaming/rest/VenteRest.java
@@ -0,0 +1,59 @@
+package fr.adaming.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.ClasseStd;
+import fr.adaming.model.Vente;
+import fr.adaming.service.IClasseStdService;
+import fr.adaming.service.IVenteService;
+
+@RestController
+public class VenteRest {
+	
+	@Autowired
+	private IVenteService venteService;
+	
+	
+	//
+	@Autowired
+	private IClasseStdService classeService;
+	
+	@RequestMapping(value="/listeVentes", method = RequestMethod.GET, produces = "application/json")
+	public List<Vente> getAllVente() {
+		return venteService.getAllVente();
+	}
+
+	@RequestMapping(value="/vente", method = RequestMethod.GET, produces = "application/json")
+	public Vente getVenteById(@RequestParam("pIdVente")int idVente) {
+		return venteService.getVenteById(idVente);
+	}
+	
+	@RequestMapping(value="/vente", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+	public Vente updateVente(@RequestBody Vente vente) {
+		return venteService.updateVente(vente);
+	}
+	
+	@RequestMapping(value="/vente", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Vente addVente(@RequestBody Vente vente) {
+		return venteService.addVente(vente, vente.getClasseStd().getType_bien());
+	}
+	
+	@RequestMapping(value="/vente", method=RequestMethod.DELETE)
+	public int deleteVente(@RequestParam("pIdVente")int idVente) {
+		return venteService.deleteVente(idVente);
+	}
+	
+	@RequestMapping(value="/listeVentesByClasse", method = RequestMethod.GET, produces = "application/json")
+	public List<Vente> getVentesByClasseStd(@RequestParam("pIdClasse")int idClasse) {
+		ClasseStd classe = classeService.getClasseStdById(idClasse);
+		return venteService.getVentesByClasseStd(classe);
+	}
+
+}
