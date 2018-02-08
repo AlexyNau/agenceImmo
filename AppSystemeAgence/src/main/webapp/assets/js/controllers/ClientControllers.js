9c7ec3beab98d71cdad8b09be4c08266f4c0042a
emegu <emegu@192.168.1.173>
2018-02-08 11:44:37 +0100

    emeric commit ClientController

--- AppSystemeAgence/src/main/webapp/assets/js/controllers/ClientControllers.js
+++ AppSystemeAgence/src/main/webapp/assets/js/controllers/ClientControllers.js
@@ -70,7 +70,7 @@ monApp
 
 .controller("mapCtrl", function($scope, clientService, $location) {
 
-		$scope.rechercheZone='';
+		$scope.rechercheZone='france';
 		
 		var adresse=["24 rue crebillon nantes",
 				"Parc des Chantiers, Boulevard Léon Bureau, 44200 Nantes",
@@ -102,27 +102,10 @@ monApp
 			   
 			  });
 
-			   $scope.map.addMarker({
-				      lat: 44.870000,
-				      lng: -0.590000,
-				      title: 'Notre entreprise',
-				      infoWindow: {
-				        content: 'Piscines de qualité '
-				      }
-				  });
-			   $scope.map.addMarker({
-				      lat: 43.870000,
-				      lng: -0.790000,
-				      title: 'hsfhe',
-				      infoWindow: {
-				        content: 'Piscines de qgjgfjgj '
-				      }
-				  });
 
 		
 	$scope.rechercher=function(){
-		
-	 
+			   
 		 console.log("Initialize")
 		 
 		 $scope.map = new GMaps({
@@ -131,71 +114,72 @@ monApp
 			    lng: -1.5276831,
 			    zoom:7, 
 			   
-			  });
-		 
-		   $scope.map.addMarker({
-			      lat: 54.870000,
-			      lng: -0.990000,
-			      title: 'Notre entreprise 222',
-			      infoWindow: {
-			        content: 'Piscines de qualité '
-			      }
-			  });
-		   $scope.map.addMarker({
-			      lat: 53.870000,
-			      lng: -0.990000,
-			      title: 'hsfhe 3333',
-			      infoWindow: {
-			        content: 'Piscines de qgjgfjgj '
-			      }
-			  });
-
-		
+			  });		 		
 		
 	console.log("Recherche="+$scope.rechercheZone);
 	var positionRecup='';
-		for(var i=0;i<2;i++){
-			console.log("Adresse :"+adresse[i])
-		geocoder1.geocode( { 'address': $scope.rechercheZone}, function(results, status) {
-			
+	var latitude=[];
+	var longitude=[];
+	var positionRecup=[];
+				
+			geocoder1.geocode( { 'address': $scope.rechercheZone}, function(results, status) {
+				console.log("i="+i)
 		      if (status == 'OK') {
-		    	  
-		       // $scope.map.setCenter(results[0].geometry.location);
-		        $scope.map.setZoom(12);
+		    	  console.log("!!!!!!!Zone Correcte !!!!!!!!!")
+		       //results[0].geometry.location.lat(), results[0].geometry.location.lng());
+		        $scope.map.setCenter({lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()});
+		        
+		    	  $scope.map.setZoom(12);
+		    	  if($scope.rechercheZone=='france')
+		    		  $scope.map.setZoom(5);
+		      }//fin geocoder 1
+				
+				for(var i=0;i<6;i++){
+					console.log("i="+i)
+					$scope.j=i
 		        geocoder2.geocode( { 'address': adresse[i]}, function(results1, status1) {
 		        	if (status1 == 'OK') {
-		        		positionRecup=results1[0].geometry.location;
-		        		console.log("Position"+positionRecup)
-		        		console.log("Position[0]="+positionRecup+" / Position[1]="+positionRecup[1])
-		        		console.log("Adresse :"+adresse[i] +"/ ")
-		        		/* $scope.map.addMarker({
-		        			
-		        			   lat: 53.870000,
-		   			           lng: -0.990000,	
-		        			//	map:$scope.map,
-		        				title:"Trop fort"
-		        			}
-			        					        				
-		        		);*/
-
-		        		
+		        		console.log("!!!!!!!Ville Find !!!!!!!!!")
 	
-		        	}//fin if geocoder2
+		        		console.log("j geocoder 2="+$scope.j)
+		        		positionRecup[i]=results1[0].geometry.location;
+		        		 latitude[i] = results1[0].geometry.location.lat();
+		                 longitude[i] = results1[0].geometry.location.lng();
+		        		console.log("Position==="+positionRecup)
+		        		console.log("latitude["+i+"]="+latitude[i]+" / longitude["+i+"]="+longitude[i])
+		        		console.log("Adresse :"+adresse[i] +"/ ")
+		        		ajoutMarker(i,latitude[i],longitude[i],adresse[i]);	
+
+	
+		        	}//fin if geocoder2		        	
 		        	
-		        })
-		       /* var myLatLng = {lat: positionRecup[0], lng: positionRecup[1]};
-		        var marker = new google.maps.Marker({
-        			position: myLatLng,
-        			map: map,
-        			title:"Trop fort"
-        		});*/
-		      }//fin if geocoder1
+		          }) //fin geocoder 2
+		          
+				}
+				
+
+		       
 		       
 		  })
-		}//fin for	
-
-	  }//fin scope function
+		}//fin for
 		
-	});	
 
+		
+		function ajoutMarker(i,lat,lg,adresse){
+			console.log("i="+i+" //// Ajout Marker lat="+lat)
+			 $scope.map.addMarker({   			
+  			   lat: lat,
+			    lng: lg,	
+  				title:adresse
+  			})
+	
+			
+
+	  }//fin scope function	
+		
+		
+		$scope.rechercher();
+		
+		
+	});
 	
